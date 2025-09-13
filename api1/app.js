be4c289367b8e43ffd685d6d3d5e658303a2df7d
khaled <khaled@localhost.localdomain>
2025-09-13 11:15:29 +0300

    update

---
+++ api1/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from API 1");
+});
+
+app.listen(port, () => {
+  console.log(`API 1 running on port ${port}`);
+});
+
